af1de03036551fd0db5a7047a948a90c5bad5c69
evgenjnr <evgeniyr7@gmail.com>
2024-07-06 13:09:15 +0300

    replace main page content to lib top-slider component

---
+++ libs/components/src/lib/top-slider/top-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-top-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-slider.component.html',
+  styleUrl: './top-slider.component.scss'
+})
+export class TopSliderComponent {
+
+}
--- projects/public-pages/src/app/pages/main-page/main-page.component.ts
+++ projects/public-pages/src/app/pages/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TopSliderComponent } from '@shared/components';
 
 @Component({
   selector: 'public-main-page',
   standalone: true,
-  imports: [],
+  imports: [TopSliderComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
