52aa8e45023407c50e76a5137b4c94da4db3e07d
evgenjnr <evgeniyr7@gmail.com>
2024-08-14 19:59:13 +0300

    add popup to view product image

--- libs/components/src/lib/prodcut-card/prodcut-card.component.ts
+++ libs/components/src/lib/prodcut-card/prodcut-card.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  ComponentPortal,
+  DomPortal,
+  Portal,
+  TemplatePortal,
+  PortalModule,
+  CdkPortalOutlet,
+} from '@angular/cdk/portal';
 
 @Component({
   selector: 'lib-prodcut-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    PortalModule,
+    CdkPortalOutlet,
+  ],
   templateUrl: './prodcut-card.component.html',
   styleUrl: './prodcut-card.component.scss',
 })
-export class ProdcutCardComponent {}
+export class ProdcutCardComponent {
+  @ViewChild('templatePortalContent')
+  templatePortalContent!: TemplateRef<unknown>;
+  @ViewChild('selectedPortal', { read: CdkPortalOutlet })
+  selectedPortal!: CdkPortalOutlet;
+  componentPortal!: ComponentPortal<any>;
+  templatePortal!: TemplatePortal<any>;
+  domPortal!: DomPortal<any>;
+
+  private _viewContainerRef: ViewContainerRef = inject(ViewContainerRef);
+  viewProductImage() {
+    if (null == this.templatePortal)
+      this.templatePortal = new TemplatePortal(
+        this.templatePortalContent,
+        this._viewContainerRef
+      );
+    this.selectedPortal.attach(this.templatePortal);
+  }
+
+  closePopup() {
+    this.selectedPortal.detach();
+  }
+}
